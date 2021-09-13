7ee946f546355a566724fc1331b077aa4e7bf629
64010441- Nipapatr Kammun <88500518+im-Elizabella@users.noreply.github.com>
2021-09-14 00:29:07 +0700

    Update week4_2.c

--- week4_2.c
+++ week4_2.c
@@ -1,26 +1,26 @@
 #include<stdio.h>
 int main ()
 {
-    int h[10] = {150,175,172,168,181,164,153,163,157,178};
-    int i,n;
-    double avg = 0.0;
-    double sum = 0.0;
-    printf("Student 1  = %d cm\n",h[0]);
-    printf("Student 2  = %d cm\n",h[1]);
-    printf("Student 3  = %d cm\n",h[2]);
-    printf("Student 4  = %d cm\n",h[3]);
-    printf("Student 5  = %d cm\n",h[4]);
-    printf("Student 6  = %d cm\n",h[5]);
-    printf("Student 7  = %d cm\n",h[6]);
-    printf("Student 8  = %d cm\n",h[7]);
-    printf("Student 9  = %d cm\n",h[8]);
-    printf("Student 10 = %d cm\n",h[9]);
-    for (i=0 ; i<10 ; ++i)
+    double h[10] = {150.30,175.40,172.50,168.00,181.55,164.45,153.00,163.75,157.60,178.00};
+    int i;
+    double avg = 0.00;
+    double sum = 0.00;
+    printf("Student 1  = %.2lf cm\n",h[0]);
+    printf("Student 2  = %.2lf cm\n",h[1]);
+    printf("Student 3  = %.2lf cm\n",h[2]);
+    printf("Student 4  = %.2lf cm\n",h[3]);
+    printf("Student 5  = %.2lf cm\n",h[4]);
+    printf("Student 6  = %.2lf cm\n",h[5]);
+    printf("Student 7  = %.2lf cm\n",h[6]);
+    printf("Student 8  = %.2lf cm\n",h[7]);
+    printf("Student 9  = %.2lf cm\n",h[8]);
+    printf("Student 10 = %.2lf cm\n",h[9]);
+    for (i=0 ; i<10 ; i++)
         {  
             sum += h[i];
         }
-        avg = sum / 10.0;
-        printf ("Average Height is %lf cm",avg);  
+        avg = sum / 10.00;
+        printf ("Average Height is %.2lf cm",avg);  
        
     return  0;
 }
