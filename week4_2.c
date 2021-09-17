2d8d466a795fd8392a6a0877809d01adf374de24
64010441- Nipapatr Kammun <88500518+im-Elizabella@users.noreply.github.com>
2021-09-17 22:06:57 +0700

    Update week4_2.c

--- week4_2.c
+++ week4_2.c
@@ -1,26 +1,44 @@
 #include<stdio.h>
+int check_number (int number)
+{
+      int count = 0;
+      int i;
+
+  for( i=2; i<=number/2; i++)
+  {
+     if(number%i == 0)
+     {
+       count=1;
+       break;
+     }
+  }
+
+  if(number == 1) 
+  {
+      count = 1;
+  }
+  if(number == 0)
+  {
+      count = 1;
+  }
+
+  return count;
+
+}
+
 int main ()
 {
-    double h[10] = {150.30,175.40,172.50,168.00,181.55,164.45,153.00,163.75,157.60,178.00};
-    int i;
-    double avg = 0.00;
-    double sum = 0.00;
-    printf("Student 1  = %.2lf cm\n",h[0]);
-    printf("Student 2  = %.2lf cm\n",h[1]);
-    printf("Student 3  = %.2lf cm\n",h[2]);
-    printf("Student 4  = %.2lf cm\n",h[3]);
-    printf("Student 5  = %.2lf cm\n",h[4]);
-    printf("Student 6  = %.2lf cm\n",h[5]);
-    printf("Student 7  = %.2lf cm\n",h[6]);
-    printf("Student 8  = %.2lf cm\n",h[7]);
-    printf("Student 9  = %.2lf cm\n",h[8]);
-    printf("Student 10 = %.2lf cm\n",h[9]);
-    for (i=0 ; i<10 ; i++)
-        {  
-            sum += h[i];
-        }
-        avg = sum / 10.00;
-        printf ("Average Height is %.2lf cm",avg);  
-       
-    return  0;
+    int number;
+    printf("Enter numer : ");
+    scanf("%d",&number);
+    if(check_number(number) == 0)
+    {
+        printf("%d is prime number", number);
+    }
+    else
+    {
+     printf("%d is not prime number", number); 
+    }
+  
+  return 0;
 }
